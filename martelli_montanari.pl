d2c87f8cf1ca0f0758794d683c64c1cf87c11388
victord54 <dallevictor@gmail.com>
2023-12-08 13:41:09 +0100

    format

--- martelli_montanari.pl
+++ martelli_montanari.pl
@@ -20,28 +20,28 @@ echo(_).
 % ====== Prédicat regle(E,R) : définit la règle de transformation R qui s'applique à l'équation E.
 regle(X ?= Y, rename) :- var(X), var(Y).
 
-regle(X ?= Y, simplify) :- 
-    var(X), atomic(Y) ; 
+regle(X ?= Y, simplify) :-
+    var(X), atomic(Y) ;
     atomic(X), atomic(Y), X == Y.
 
-regle(X ?= Y, expand) :- 
-    var(X), compound(Y), 
+regle(X ?= Y, expand) :-
+    var(X), compound(Y),
     \+occur_check(X,Y).
 
-regle(X ?= Y, check) :- 
+regle(X ?= Y, check) :-
     var(X), occur_check(X, Y), (X \== Y).
 
 regle(X ?= Y, orient) :- nonvar(X), var(Y).
 
 % A et B = nom de la fonction, M et N = ariétés
-regle(X ?= Y, decompose) :- 
-    compound(X), compound(Y), 
-    functor(X,A,M), functor(Y,B,N), 
+regle(X ?= Y, decompose) :-
+    compound(X), compound(Y),
+    functor(X, A, M), functor(Y, B, N),
     (A == B), (M == N).
 
-regle(X ?= Y, clash) :- 
-    compound(X), compound(Y), 
-    functor(X,A,M), functor(Y,B,N), 
+regle(X ?= Y, clash) :-
+    compound(X), compound(Y),
+    functor(X, A, M), functor(Y, B, N),
     (A \== B ; M \== N).
 
 % =================================================================================================
@@ -56,50 +56,56 @@ reduit(simplify, X ?= Y, P, Q) :-  X = Y, Q = P.
 
 reduit(expand, X ?= Y, P, Q) :-  X = Y, Q = P.
 
-reduit(check, _, _, _) :-  
+reduit(check, _, _, _) :-
     write('Occurrence - unification impossible\n'), fail.
 
 reduit(orient, X ?= Y, P, Q) :- Q = [Y ?= X|P].
 
-reduit(decompose, X ?= Y, P, Q) :- 
-    X =.. [_|L1], Y =.. [_|L2], 
+reduit(decompose, X ?= Y, P, Q) :-
+    X =.. [_|L1], Y =.. [_|L2],
     decomposition(L1, L2, R), append(R, P, Q).
 
-reduit(clash,_, _, _) :- 
+reduit(clash, _, _, _) :-
     write('Clash - unification impossible\n'), fail.
 
-decomposition([H1|T1], [H2|T2], R) :- 
+decomposition([H1|T1], [H2|T2], R) :-
     decomposition(T1, T2, S), append([H1 ?= H2], S, R).
-decomposition([], [], R) :- R=[].
+decomposition([], [], R) :- R = [].
 
 % =================================================================================================
 % ====== Prédicat unifie(P) : unifie le système d'équations P où P est une liste d'équations.
 
 unifie([H|T]) :- unifie([H|T], choix_premier).
-unifie([H|T], choix_premier) :- 
-    print_system([H|T]), choix_premier(T,Q , H, _), 
-    unifie(Q, choix_premier),!.
-unifie(P, choix_pondere_1) :- 
-    print_system(P), choix_pondere_1(P,Q, _, _), 
-    unifie(Q, choix_pondere_1),!.
-unifie(P, choix_pondere_2) :- 
-    print_system(P), choix_pondere_2(P,Q, _, _), 
-    unifie(Q, choix_pondere_2),!.
-unifie([], _) :- 
+
+unifie([H|T], choix_premier) :-
+    print_system([H|T]), choix_premier(T, Q, H, _),
+    unifie(Q, choix_premier), !.
+
+unifie(P, choix_pondere_1) :-
+    print_system(P), choix_pondere_1(P, Q, _, _),
+    unifie(Q, choix_pondere_1), !.
+
+unifie(P, choix_pondere_2) :-
+    print_system(P), choix_pondere_2(P, Q, _, _),
+    unifie(Q, choix_pondere_2), !.
+
+unifie([], _) :-
     write("Unification réussie ! Voici le résultat : "), nl.
 
-choix_premier(P, Q , E ,R) :- 
-    regle(E, R),print_regle(R,E), reduit(R, E, P, Q).
+% =================================================================================================
+% ====== Prédicat choix_premier(P, Q, E, R) : choisit la première règle R qui s'applique à l'équation E du système P.
+choix_premier(P, Q, E, R) :- 
+    regle(E, R),print_regle(R, E), reduit(R, E, P, Q).
 
-choix_pondere_1(P, Q, _, _) :- 
+choix_pondere_1(P, Q, _, _) :-
     choix_equation(P, [clash, check, rename, simplify, orient, decompose, expand], E, R),
-    print_regle(R,E),
+    print_regle(R, E),
     select(E, P, N),
     reduit(R, E, N, Q).
 
-choix_pondere_2(P, Q, _, _) :- 
+choix_pondere_2(P, Q, _, _) :-
     choix_equation(P, [decompose, expand, orient, simplify, rename, check, clash], E, R),
-    print_regle(R,E),
+    print_regle(R, E),
     select(E, P, N),
     reduit(R, E, N, Q).
 
@@ -107,21 +113,21 @@ choix_equation(P , [R_TESTE | RESTE_LR], E, R) :-
     choix_equation_aux(P, R_TESTE, E, R), !;
     choix_equation(P, RESTE_LR, E, R).
 
-choix_equation_aux([E_TESTE | RESTE_P], R_TESTE , E, R) :-
-    (   regle( E_TESTE, R_TESTE)
-    ->  E = E_TESTE, R = R_TESTE ,! 
-    ;   choix_equation_aux(RESTE_P, R_TESTE, E, R) 
+choix_equation_aux([E_TESTE | RESTE_P], R_TESTE, E, R) :-
+    (   regle(E_TESTE, R_TESTE)
+    ->  E = E_TESTE, R = R_TESTE , !
+    ;   choix_equation_aux(RESTE_P, R_TESTE, E, R)
     ).
-choix_equation_aux(_,[],_,_).
+choix_equation_aux(_, [], _, _).
 
 print_system(P) :- echo('system : '), echo(P), echo('\n').
-print_regle(R,E) :- echo(R),echo(' : '), echo(E), echo('\n').  
+print_regle(R, E) :- echo(R),echo(' : '), echo(E), echo('\n').
 
 unif(P) :- unif(P, choix_premier).
-unif(P,C) :- clr_echo, unifie(P,C).
+unif(P, C) :- clr_echo, unifie(P, C).
 trace_unif(P) :- trace_unif(P, choix_premier).
-trace_unif(P,C) :- 
-    set_echo, echo('Stratégie : '), echo(C), nl, nl, unifie(P,C).
+trace_unif(P, C) :-
+    set_echo, echo('Stratégie : '), echo(C), nl, nl, unifie(P, C).
 
-trace_temps(P,C) :- statistics(cputime, T0), unif(P,C), statistics(cputime, T1), T is T1 - T0, format('CPU time: ~w~n', [T]).
+trace_temps(P, C) :- statistics(cputime, T0), unif(P, C), statistics(cputime, T1), T is T1 - T0, format('CPU time: ~w~n', [T]).
 
